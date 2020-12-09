1b09da9d97e13b035844cdda6c48dc593a509eda
Pascal Fodiman <pascal@fodiman.fr>
2020-12-09 16:50:19 +0100

    add seeds, Flat model+controller, missing views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Flat.destroy_all
+flat_names = [ "Bed setting room in Galery Lafayette Street",
+               "Studio tout confort proche Gare du nord",
+               "Human home",
+               "Luxueux nid d'amour au coeur de Paris",
+               "Luxury Studio Bastille BAIL MOBILITÉ POSSIBLE" ]
+5.times do |i|
+  Flat.create(
+    name: flat_names[i],
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: true),
+    price_per_night: rand(40..200).to_i,
+    number_of_guests: [1, 2, 3, 4, 5].sample
+  )
+end
+
+puts "Finished!"
